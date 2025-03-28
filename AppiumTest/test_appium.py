d6a8d59b371ce5d5a6355d51c1873b8d509845c6
mmol93 <ljws93@naver.com>
2025-03-28 11:26:58 +0900

    1. add scroll feature to find element 2. separate test detail

--- AppiumTest/test_appium.py
+++ AppiumTest/test_appium.py
@@ -4,6 +4,9 @@ from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.webdriver import WebDriver
 from dotenv import load_dotenv
+from selenium.common import NoSuchElementException
+from selenium.webdriver.remote.webelement import WebElement
+
 from utils import get_connected_device
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,14 +16,17 @@ from appium.webdriver.common.appiumby import AppiumBy
 class TestAppium(unittest.TestCase):
     appium_server_url = 'http://127.0.0.1:4723/wd/hub'
     driver: WebDriver
+    view_waiter: WebDriverWait
 
     def setUp(self):
         load_dotenv()
         platform, uuid = get_connected_device()
         if platform == "Android":
             self.driver = self.setup_android_driver()
+            self.view_waiter = WebDriverWait(self.driver, 7)
         elif platform == "iOS":
             self.driver = self.setup_ios_driver(ios_uuid=uuid)
+            self.view_waiter = WebDriverWait(self.driver, 7)
         else:
             print("Can't find connected device.")
             quit()
@@ -59,14 +65,76 @@ class TestAppium(unittest.TestCase):
 
         return webdriver.Remote(self.appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
 
-    def tearDown(self) -> None:
+    def tear_down(self) -> None:
         if self.driver:
             self.driver.quit()
 
+    def wait_for_view(self, view_id=None, view_xpath=None):
+        """ wait for certain view """
+        if view_id:
+            self.view_waiter.until(EC.visibility_of_element_located((AppiumBy.ID, view_id)))
+        else:
+            self.view_waiter.until(EC.visibility_of_element_located((AppiumBy.XPATH, view_xpath)))
+
+    def is_scrollable(self):
+        try:
+            scrollable_element = self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
+                                                          'new UiSelector().scrollable(true)')
+            scrollable_state = scrollable_element.get_attribute("scrollable")
+            return scrollable_state == "true"
+        except:
+            return False
+
+    def scroll_screen(self):
+        """scroll the display"""
+        scrollable = 'new UiScrollable(new UiSelector().scrollable(true))'
+        scroll_command = f'{scrollable}.scrollForward()'
+        self.driver.execute_script("mobile: scroll", {"strategy": "androidUiAutomator", "selector": scroll_command})
+
+    def scroll_to_element(self, view_id=None, view_xpath=None, max_attempts=4) -> WebElement:
+        """try to find element with scrolling"""
+        attempts = 0
+
+        while attempts < max_attempts:
+            try:
+                if view_id:
+                    element = self.driver.find_element(AppiumBy.ID, view_xpath)
+                else:
+                    element = self.driver.find_element(AppiumBy.XPATH, view_xpath)
+
+                if element.is_displayed():
+                    return element
+            except NoSuchElementException:
+                # if it fails to find element, scroll again.
+                self.scroll_screen()
+            attempts += 1
+        raise Exception(f"Unable to find the element with identifier {view_id or view_xpath} after {max_attempts} attempts.")
+
+    def get_view(self, view_id=None, view_xpath=None) -> WebElement:
+        """
+        wait for certain view and return it for next action
+        1. wait for it until 7 seconds.
+            * find scroll element and scroll it for 4 times unit find it.
+        2. if it finds element, return it
+            * if it can't find element, raise exception.
+        """
+        if view_id:
+            # ex) view_id=example_text_view
+            try:
+                self.view_waiter.until(EC.visibility_of_element_located((AppiumBy.ID, view_id)))
+                return self.driver.find_element(AppiumBy.ID, view_id)
+            except :
+                return self.scroll_to_element(view_id=view_id)
+
+        else:
+            # ex) Android: view_xpath="//*[@text="ご利用明細"]"  iOS: "//*[@name="ご利用明細"]"
+            try:
+                self.view_waiter.until(EC.visibility_of_element_located((AppiumBy.XPATH, view_xpath)))
+                return self.driver.find_element(AppiumBy.XPATH, view_xpath)
+            except:
+                return self.scroll_to_element(view_id=view_id)
+
+
     def test_start(self):
         # App test visualize
-        self.driver.activate_app(os.getenv('ANDROID_APP_PACKAGE_NAME'))
-
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.visibility_of_element_located((AppiumBy.ID, 'login_viewpager')))
-        self.driver.find_element(AppiumBy.ID, 'login_fragment_reg_button').click()
+        print("not yet here")
