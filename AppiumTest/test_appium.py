42c5febfec2bffe010e61093269a6d57a0948768
mmol93 <ljws93@naver.com>
2025-03-28 13:27:46 +0900

    1. (bug) fix try~catch 2. (bug) fix scroll function feature(to Touch Action)

--- AppiumTest/test_appium.py
+++ AppiumTest/test_appium.py
@@ -4,14 +4,15 @@ from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.webdriver import WebDriver
 from dotenv import load_dotenv
-from selenium.common import NoSuchElementException
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.remote.webelement import WebElement
-
 from utils import get_connected_device
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from appium.webdriver.common.appiumby import AppiumBy
-
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+from selenium.webdriver.common.actions import interaction
 
 class TestAppium(unittest.TestCase):
     appium_server_url = 'http://127.0.0.1:4723/wd/hub'
@@ -82,14 +83,32 @@ class TestAppium(unittest.TestCase):
                                                           'new UiSelector().scrollable(true)')
             scrollable_state = scrollable_element.get_attribute("scrollable")
             return scrollable_state == "true"
-        except:
+        except Exception as e:
             return False
 
     def scroll_screen(self):
-        """scroll the display"""
-        scrollable = 'new UiScrollable(new UiSelector().scrollable(true))'
-        scroll_command = f'{scrollable}.scrollForward()'
-        self.driver.execute_script("mobile: scroll", {"strategy": "androidUiAutomator", "selector": scroll_command})
+        """scroll the display using TouchAction"""
+        # get current display size
+        screen_size = self.driver.get_window_size()
+        width = screen_size['width']
+        height = screen_size['height']
+
+        # get center coordinates
+        center_x = width // 2
+        center_y = height // 2
+
+        # scroll end point (to down)
+        end_y = int(height * 0.2)
+
+        # do W3C action
+        actions = ActionChains(self.driver)
+        actions.w3c_actions = ActionBuilder(self.driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
+        actions.w3c_actions.pointer_action.move_to_location(center_x, center_y)
+        actions.w3c_actions.pointer_action.pointer_down()
+        actions.w3c_actions.pointer_action.pause(0.5)
+        actions.w3c_actions.pointer_action.move_to_location(center_x, end_y)
+        actions.w3c_actions.pointer_action.pointer_up()
+        actions.perform()
 
     def scroll_to_element(self, view_id=None, view_xpath=None, max_attempts=4) -> WebElement:
         """try to find element with scrolling"""
@@ -104,7 +123,7 @@ class TestAppium(unittest.TestCase):
 
                 if element.is_displayed():
                     return element
-            except NoSuchElementException:
+            except Exception as e:
                 # if it fails to find element, scroll again.
                 self.scroll_screen()
             attempts += 1
@@ -123,16 +142,16 @@ class TestAppium(unittest.TestCase):
             try:
                 self.view_waiter.until(EC.visibility_of_element_located((AppiumBy.ID, view_id)))
                 return self.driver.find_element(AppiumBy.ID, view_id)
-            except :
+            except Exception as e:
                 return self.scroll_to_element(view_id=view_id)
 
         else:
-            # ex) Android: view_xpath="//*[@text="ご利用明細"]"  iOS: "//*[@name="ご利用明細"]"
+            # ex) Android: view_xpath="//*[@text="text_name"]"  iOS: "//*[@name="text_name"]"
             try:
                 self.view_waiter.until(EC.visibility_of_element_located((AppiumBy.XPATH, view_xpath)))
                 return self.driver.find_element(AppiumBy.XPATH, view_xpath)
-            except:
-                return self.scroll_to_element(view_id=view_id)
+            except Exception as e:
+                return self.scroll_to_element(view_xpath=view_xpath)
 
 
     def test_start(self):
